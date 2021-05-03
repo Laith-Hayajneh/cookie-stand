87d5402cf8f41db696a4f22f03952c5abd3dd2a4
laith-hayajneh <farfesh96@gmail.com>
2021-05-03 11:14:00 +0300

    adding lab06

--- js/app.js
+++ js/app.js
@@ -6,49 +6,47 @@ let seattle = {
   minCusPerHour: 23,
   maxCusPerHour: 65,
   avgCookiesCusPerHour: 6.3,
-  // numOfCusPerHour: 0,
-  numOfCusPerHour: [],
+  ramdomCustNumber: [],
+  salesPerHour: [],
+  totalPerHour:0,
 
 
-//random Number
-  getRan: function (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    this.numOfCusPerHour = Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  //the multiply 
-  cookiesPerHour:function(){
-    return(this.avgCookiesCusPerHour * this.addingCoutPerHour())},
+  //random Number
 
-  addingCoutPerHour: function () {
-    for (i = 0; i < workingHour.length; i++) {
-      this.numOfCusPerHour.push(this.getRan(this.minCusPerHour, this.maxCusPerHour));
-      console.log(this.numOfCusPerHour)
+
+  randomCustNumber: function () {
+    for (let hour = 0; hour < workingHour.length; hour++) {
+
+      this.ramdomCustNumber.push(Math.floor(Math.random() * (this.maxCusPerHour - this.minCusPerHour + 1) + min));
     }
+
   },
+  salesCookies: function () {
+    for (let i = 0; i < this.workingHour.length; index++) {
+      this.salesPerHour.push(Math.ceil( this.randomCustNumber[i] * this.avgCookiesCusPerHour));
+      this.totalPerHour+=this.salesPerHour;
+
+    }
+
+  },
+  render:function(){
+    let container =document.getElementById('sales;');
+    let h2E1 =document,createElement('h2');
+    container.appendChild(h2E1);
+    h2E1.textContent= this.storeName;
+    let ulE1 =document.createElement('ul');
+    container.appendChild(ulE1);
+    for (let hour = 0; hour < array.length; hour++) {
+      let liE1 =document.createElement('li');
+      ulE1.appendChild(liE1),
 
 
 
+      
+
+    },
+
+  },
 }
 
-seattle.getRan(1,9);
 
-
-
-
-
-
-
-// seattle.render();
-
-//   //product 
-  // simulated: numOfCusPerHour * avgCookiesCusPerHour,
-  // render: function () {
-  //   let divEl = document.getElementById('storNAme');
-
-  //   let articleEl = document.createElement('article');
-  //   divEl.appendChild(articleEl);
-
-  //   let h2El = document.createElement('h2');
-  //   h2El.textContent = this.storeName;
-  //   articleEl.appendChild(h2El),
